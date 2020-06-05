0126812c6047ff2f66f542770c27e1dbf4758115
unknown <suys0101@gmail.com>
2020-06-05 10:06:48 +0900

    initial

---
+++ nunu/Assets/ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
